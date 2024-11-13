3b44a86e4cab4118741093442ee2823e287cf41a
KarlMcCarthyMoran <karljm05@gmail.com>
2024-11-13 22:00:31 +0000

    lab 9 part 4 commit

--- books_project/settings.py
+++ books_project/settings.py
@@ -132,6 +132,9 @@ STATICFILES_FINDERS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
